97b6828a458f67c0818c767579bd3f24881f6b79
PedroBueno <pedrovieirabueno@icloud.com>
2026-05-20 12:24:28 -0300

    desafio-objeto

---
+++ ex01/script.js
@@ -0,0 +1,28 @@
+const alunos = [ {
+    nome: "Ana",
+    idade: 16,
+    disciplinaPreferida: "Matemática"
+},
+{
+    nome: "Bruno",
+    idade: 17,
+    disciplinaPreferida: "História"
+},
+{
+    nome: "Carlos",
+    idade: 15,
+    disciplinaPreferida: "Geografia"
+},
+{
+    nome: "Diana",
+    idade: 16,
+    disciplinaPreferida: "Biologia"
+},
+{
+    nome: "Eduardo",
+    idade: 17,
+    disciplinaPreferida: "Física"
+}
+];
+
+alert("Nome:" + alunos[0].nome + "\nDisciplina Preferida: " + alunos[4].disciplinaPreferida);
